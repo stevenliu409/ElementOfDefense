05a9bf3807590a83414899080cd7d8d791c539df
Steven <steven.github@gmail.com>
2013-05-01 13:35:17 -0700

    hello test -> hello steven

--- Element Of Defense/HelloWorldLayer.m
+++ Element Of Defense/HelloWorldLayer.m
@@ -42,7 +42,7 @@
 	if( (self=[super init]) ) {
 		
 		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello TEST" fontName:@"Marker Felt" fontSize:64];
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello Steven" fontName:@"Marker Felt" fontSize:64];
 
 		// ask director for the window size
 		CGSize size = [[CCDirector sharedDirector] winSize];
