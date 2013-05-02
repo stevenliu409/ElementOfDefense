a8028527c5edc9a2609869e2798708032805c534
ccandy <ccunandy@yahoo.com>
2013-05-02 10:08:47 -0700

    Andy checkin

--- Element Of Defense/HelloWorldLayer.m
+++ Element Of Defense/HelloWorldLayer.m
@@ -42,7 +42,7 @@
 	if( (self=[super init]) ) {
 		
 		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello Steven" fontName:@"Marker Felt" fontSize:64];
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello Steven Andy is here" fontName:@"Marker Felt" fontSize:64];
 
 		// ask director for the window size
 		CGSize size = [[CCDirector sharedDirector] winSize];
