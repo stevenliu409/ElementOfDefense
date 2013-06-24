d265ec5dd764320fc41a5b6c775ac15e2fa2f455
ccandy <ccunandy@yahoo.com>
2013-06-24 13:45:47 -0700

    add monster wave class

---
+++ mWave.h
@@ -0,0 +1,19 @@
+//
+//  mWave.h
+//  ZombieDefense
+//
+//  Created by Hua Dong on 13-06-24.
+//
+//
+
+#import "CCNode.h"
+
+@interface mWave : CCNode{
+    
+}
+
+
+
+
+
+@end
---
+++ mWave.m
@@ -0,0 +1,15 @@
+//
+//  mWave.m
+//  ZombieDefense
+//
+//  Created by Hua Dong on 13-06-24.
+//
+//
+
+#import "mWave.h"
+
+@implementation mWave
+
+
+
+@end
